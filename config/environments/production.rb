efacacd0fdafecca2c4621bf1cf7837b3091867a
Tigran Asriyan <tigran.asriyan@gmail.com>
2013-10-24 22:40:21 -0400

    add amazon s3 for paperclick uploads to heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,6 +78,16 @@ SampleApp::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
   # Note to set this to your actual host
-config.action_mailer.default_url_options = { :host => 'aturface.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'aturface.herokuapp.com' }
+
+  # Sets Paperclip to upload images to Amazon S3
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['AWS_BUCKET'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+      }
+    }
 
 end
